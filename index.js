44d3bbe444e129a0f4712e7952b9003dc37244e8
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-29 22:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function dec(n) {
   return n -= 1
 }
 function makeInt(n) {
-parseInt(n, 10)
+n=parseInt(n, 10)
   return n
 }
 function preserveDecimal(n) {
-  parseFloat(n)
+  n=parseFloat(n)
 return n
 }
